ece63c9c669009978f6cdad4007abaa3ca4d9a37
KechioucheIlyes <112709633+KechioucheIlyes@users.noreply.github.com>
2023-06-04 21:21:23 +0200

    Adding Collapse

--- Kasa/src/components/InfosCards/InfosCards.jsx
+++ Kasa/src/components/InfosCards/InfosCards.jsx
@@ -1,16 +1,38 @@
 import styled from "styled-components"
+import "../../style/InfosCards/InfosCards.css"
 
 const StyledImgHost = styled.img`
         border-radius : 50%;
+        height : 64px;
     `
+const StyledTitle = styled.h1`
+    color: #fe605f;
+    font-size : 36px;
+`
+const StyledLocation = styled.p`
+    color: #fe605f;
+    font-size : 18px;
+`
+const StyledNameHost = styled.p`
+    color: #fe605f;
+    font-size : 18px;
+    width : 93px;
+`
 export function InfosCards({title , location , hostName , hostPicture }){
     
     return (
-        <div>
-            <h1>{title}</h1>
-            <h2>{location}</h2>
-            <StyledImgHost src={hostPicture} alt="text"></StyledImgHost>
-            <h3>{hostName}</h3>
+        <div className="infos-cards-container" >
+            <div >
+                <StyledTitle >{title}</StyledTitle>
+                <StyledLocation>{location}</StyledLocation>
+            </div>
+            <div className="host-container">
+                <StyledImgHost src={hostPicture} alt={`picture of ${hostName}`}></StyledImgHost>
+                <StyledNameHost>{hostName}</StyledNameHost>
+            </div>
+            
+            
+            
         </div>
     )
 }
--- Kasa/src/pages/Fiche-Logement/FicheLogement.jsx
+++ Kasa/src/pages/Fiche-Logement/FicheLogement.jsx
@@ -2,6 +2,23 @@ import { Caroussel } from "../../components/Carroussel/Carousel"
 import { useParams } from "react-router"
 import { dataLogement } from "../../utils/DataLogement/DataLogement";
 import { InfosCards } from "../../components/InfosCards/InfosCards";
+import styled from "styled-components";
+import { Collapse } from "../../components/Collapse/Collapse";
+
+
+const StyledTags = styled.div`
+    color: white;
+    background-color :#fe605f; 
+    border: 1px solid #fe605f;
+    border-radius : 10px; 
+    width: fit-content;
+    padding: 3px 41px 2px 40px;
+    margin-left: 10px;
+`
+const StyledTagsContainer = styled.div`
+    display: flex;
+    
+`
 
 export function FicheLogement (){
     const {locationId} =useParams()
@@ -12,16 +29,36 @@ export function FicheLogement (){
 
 
     return (<div>
-        <Caroussel picture={filtredElement.map((el => el.cover))} />
-        <InfosCards 
-            title={filtredElement.map((el)=>el.title)} 
-            location={filtredElement.map(el => el.location)} 
-            hostPicture={filtredElement.map(el => el.host.picture)} 
-            hostName={filtredElement.map(el => el.host.name)} 
+        {filtredElement.map((el)=>{
+            return (
+            <div>
+                <Caroussel picture= {el.cover} />
             
+                <InfosCards 
+                title={(el.title)} 
+                location={(el.location)} 
+                hostPicture={(el.host.picture)} 
+                hostName={(el.host.name)} 
+                />
+
+                <StyledTagsContainer className="tags-container">
+                    
+                    {el.tags.map(tag => <StyledTags>{tag}</StyledTags>)}
+                
+                </StyledTagsContainer>
+
+                
+            </div>
             
+            )
             
-            />
+        })}
+        
+        
+            
+        
+
+        
     </div>
     )
 }
