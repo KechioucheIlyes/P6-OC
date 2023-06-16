5b27fee76e8ddcbd1b350cce2226ab86a49e8c88
KechioucheIlyes <112709633+KechioucheIlyes@users.noreply.github.com>
2023-06-16 20:00:05 +0200

    Animations

--- Kasa/src/components/Collapse/Collapse.jsx
+++ Kasa/src/components/Collapse/Collapse.jsx
@@ -1,76 +1,115 @@
-    import "../../style/Collapse/Collapse.css"
-    import vector from "../../assets/Vector-up.svg"
-    import { useState } from "react"
-    import vectDown from "../../assets/Vector-down.svg"
-    import styled from "styled-components"
+import "../../style/Collapse/Collapse.css";
+import vector from "../../assets/Vector-up.svg";
+import { useState, useEffect } from "react";
+import styled, { keyframes } from "styled-components";
+
+const rotateAnimation = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(180deg);
+  }
+`;
+const rotateAnimationReverse = keyframes`
+  from {
+    transform: rotate(180deg);
+  }
+  to {
+    transform: rotate(0deg);
+  }
+`;
 
 
-    const StyledCollapse = styled.div`
-    padding: 10px 20px 10px 15px;
-    background-color: rgba(255, 96, 96, 1);
-    border-radius: 5px;
-    color: aliceblue;
-    margin-left: auto;
-    margin-right: auto;
-    margin-top: 38px;
-    display: flex;
-    justify-content: space-between;
-    width: ${props => props.width ? props.width : 'auto'};
-    border: 1px solid black;
-    @media screen and (max-width : 768px){
+const StyledCollapse = styled.div`
+  padding: 10px 20px 10px 15px;
+  background-color: rgba(255, 96, 96, 1);
+  border-radius: 5px;
+  color: aliceblue;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 38px;
+  display: flex;
+  justify-content: space-between;
+  width: ${props => (props.width ? props.width : 'auto')};
+  border: 1px solid black;
+
+  img {
+    transition: transform 0.3s ease-in-out;
+    animation: ${props => (props.rotate ? rotateAnimation : rotateAnimationReverse)} 0.3s forwards;
+  }
+
+  @media screen and (max-width: 768px) {
     min-width: 82%;
-    max-width : 150px;
+    max-width: 150px;
+
     img {
-            position: relative;
-            top: 5px;
-            height: 9px;
-        }
+      position: relative;
+      top: 5px;
+      height: 9px;
     }
-    `;
-    const StyledCollapseInfos = styled.div`
-    padding: 10px 20px 10px 15px;
-    background-color: #f5f6f6;
-    border-radius: 5px;
-    color: aliceblue;
-    margin-left: auto;
-    margin-right: auto;
-    color: rgba(255, 96, 96, 1);
-    width : ${props => props.widthinfos ? props.widthinfos : 'auto' };
-    @media screen and (max-width : 768px){
-        min-width: 82%;
-        max-width : 150px;
-        border: 2px solid blue;
-        
+  }
+`;
 
+const StyledCollapseInfos = styled.div`
+  padding: 10px 20px 10px 15px;
+  background-color: #f5f6f6;
+  border-radius: 5px;
+  color: aliceblue;
+  margin-left: auto;
+  margin-right: auto;
+  color: rgba(255, 96, 96, 1);
+  width: ${props => (props.widthinfos ? props.widthinfos : 'auto')};
 
-    }
+  @media screen and (max-width: 768px) {
+    min-width: 82%;
+    max-width: 150px;
+    border: 2px solid blue;
+  }
+`;
 
-    `
-    const StyledContainer = styled.div`
-     width : ${props => props.widthinfos ? props.widthinfos : 'auto' };
-     @media screen and (max-width : 768px){
-        width : 100%;
-     }
-    `
-    export function Collapse({name, content , width , children}){
-    const [vectorUp , setVectorUp] = useState(true)
+const StyledContainer = styled.div`
+  width: ${props => (props.widthinfos ? props.widthinfos : 'auto')};
 
+  @media screen and (max-width: 768px) {
+    width: 100%;
+  }
+`;
 
-
-
-
-    return(
-        <StyledContainer widthinfos={width} >
-            <StyledCollapse  width={width} >
-                {name}
-                {vectorUp ? <img onClick={() => {setVectorUp(!vectorUp)}} src={vector} alt="vecteur-up"></img> : <img onClick={() => {setVectorUp(!vectorUp)}} src={vectDown} alt="vecteur down"></img>}
-            </StyledCollapse> 
-            {vectorUp ? null : <StyledCollapseInfos className="tes" widthinfos={width} >
-                <div >{content} {children}</div>
-            </StyledCollapseInfos> } 
-            
-            
-        </StyledContainer>
-
-    )
-    }
+export function Collapse({ name, content, width, children }) {
+    const [vectorUp, setVectorUp] = useState(true);
+    const [rotate, setRotate] = useState(false);
+    const [rotateReverse, setRotateReverse] = useState(false);
+  
+    useEffect(() => {
+      if (vectorUp) {
+        setRotate(false);
+        setRotateReverse(true);
+      } else {
+        setRotate(true);
+        setRotateReverse(false);
+      }
+    }, [vectorUp]);
+  
+    const handleClick = () => {
+      setVectorUp(!vectorUp);
+    };
+  
+    return (
+      <StyledContainer widthinfos={width}>
+        <StyledCollapse width={width} rotate={rotate} rotateReverse={rotateReverse}>
+          {name}
+          <img onClick={handleClick} src={vector} alt="vecteur" />
+        </StyledCollapse>
+        {vectorUp ? null : (
+          <StyledCollapseInfos className="tes" widthinfos={width}>
+            <div>
+              {content}
+              {children}
+            </div>
+          </StyledCollapseInfos>
+        )}
+      </StyledContainer>
+    );
+  }
+  
--- Kasa/src/components/NavBar/NavBar.jsx
+++ Kasa/src/components/NavBar/NavBar.jsx
@@ -1,36 +1,44 @@
-import { Link } from "react-router-dom"
-import logo from "../../assets/logo-kasa/LOGO.png"
-import "../../style/Navbar/NavBar.css"
-import styled from "styled-components"
-import { useEffect, useState } from "react"
+import { Link, useLocation } from "react-router-dom";
+import logo from "../../assets/logo-kasa/LOGO.png";
+import "../../style/Navbar/NavBar.css";
+import styled from "styled-components";
+import { useEffect, useState } from "react";
 
 const LinkStyled = styled(Link)`
-        color: #fe605f;
-        text-decoration : none;
-        margin:20px;
-        font-size : 27px;
-        ${(props) => props.$focused ? `border-bottom : 1px solid #fe605f`  : null }
-    `
-export function NavBar () { 
-    
-    
-    
-    return(
-        <header>
-            <div className="navbar-container">
-            
-                <img src={logo} alt="logo-Kasa" />
-                
-                <nav>
-                    <ul className="nav-ul">
-                        <li><LinkStyled to={"/"} $focused>Accueil</LinkStyled></li>
-                        <li><LinkStyled to={"/propos"}>À Propos</LinkStyled></li>
-                        
-                        
-                    </ul>
-                </nav>
-            </div>
-        </header>
-        
-    )
+  color: #fe605f;
+  text-decoration: none;
+  margin: 20px;
+  font-size: 27px;
+  ${(props) => (props.$focused ? `border-bottom: 1px solid #fe605f` : null)}
+`;
+
+export function NavBar() {
+  const location = useLocation();
+  const [currentRoute, setCurrentRoute] = useState(location.pathname);
+
+  useEffect(() => {
+    setCurrentRoute(location.pathname);
+  }, [location.pathname]);
+
+  return (
+    <header>
+      <div className="navbar-container">
+        <img src={logo} alt="logo-Kasa" />
+        <nav>
+          <ul className="nav-ul">
+            <li>
+              <LinkStyled to={"/"} $focused={currentRoute === "/"}>
+                Accueil
+              </LinkStyled>
+            </li>
+            <li>
+              <LinkStyled to={"/propos"} $focused={currentRoute === "/propos"}>
+                À Propos
+              </LinkStyled>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 }
